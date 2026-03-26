bec1667053c58154bbe76fd6802766cf9d206d4b
maybe-adamant <maybe.adamant@gmail.com>
2026-03-26 16:15:59 -0400

    removing hard dependency on coordinator mod name

--- src/main.lua
+++ src/main.lua
@@ -48,7 +48,7 @@ end
 local function registerHooks()
     modutil.mod.Path.Wrap("ShowHealthUI", function(baseFunc)
         baseFunc()
-        if lib.isEnabled(config) then
+        if lib.isEnabled(config, public.definition.modpack) then
             ShowDepthCounter()
         end
     end)
@@ -67,8 +67,8 @@ modutil.once_loaded.game(function()
     loader.load(function()
         import_as_fallback(rom.game)
         registerHooks()
-        if lib.isEnabled(config) then apply() end
-        if public.definition.dataMutation and not mods['adamant-Modpack_Core'] then
+        if lib.isEnabled(config, public.definition.modpack) then apply() end
+        if public.definition.dataMutation and not lib.isCoordinated(public.definition.modpack) then
             SetupRunData()
         end
     end)
